747f7061268c24c3c36c2c185c9115a4f93cf434
Ernest <94367979+musaubrian@users.noreply.github.com>
2023-03-03 18:17:17 +0300

    Create RedisClient and DBClient (#1)
    * chore: create class RedisClient
    
    * * Contains the common redis functions to be used.
    * * Create test_cases dir containing the checks offered.
    
    * chore: add DBClient
    
    ** This acts as a wrapper for the mogodb client
    ** Rename the test_cases files

---
+++ utils/db.js
@@ -0,0 +1,36 @@
+import MongoClient from 'mongodb/lib/mongo_client';
+
+class DBClient {
+  constructor () {
+    this.host = process.env.DB_HOST || 'localhost';
+    this.port = process.env.DB_PORT || 27017;
+    this.database = process.env.DB_DATABASE || 'files_manager';
+    this.mongoUrl = `mongodb://${this.host}:${this.port}`;
+
+    this.client = new MongoClient(this.mongoUrl, { useUnifiedTopology: true });
+    this.client.connect().then(() => {
+      this.db = this.client.db(this.database);
+    });
+  }
+
+  // Checks if the connection is successfull
+  isAlive () {
+    if (this.client.isConnected()) return true;
+    return false;
+  }
+
+  // Returns the number of documents in the collection users
+  async nbUsers () {
+    const numOfUsers = await this.db.collection('users').countDocuments();
+    return numOfUsers;
+  }
+
+  // Returns the number of documents in the collection files
+  async nbFiles () {
+    const numOfFiles = await this.db.collection('files').countDocuments();
+    return numOfFiles;
+  }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
