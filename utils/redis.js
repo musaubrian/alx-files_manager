dbad119b7bb3d8e34006dfb3452686977e5e0d41
Ernest <musaubrian15@gmail.com>
2023-03-03 17:49:49 +0300

    chore: add DBClient
    This acts as a wrapper for the mogodb client
    * Rename the test_cases files

--- utils/redis.js
+++ utils/redis.js
@@ -1,37 +1,37 @@
-import { createClient } from "redis";
-import { promisify } from "util";
+import { createClient } from 'redis';
+import { promisify } from 'util';
 
 class RedisClient {
-  constructor() {
+  constructor () {
     this.client = createClient();
-    this.client.on("error", (error) => {
+    this.client.on('error', (error) => {
       console.log(`Connection error: ${error}`);
     });
   }
 
   // Checks if the connection is successfull
-  isAlive() {
-    const success = this.client.on("connect", () => true);
+  isAlive () {
+    const success = this.client.on('connect', () => true);
     if (success) return true;
     return false;
   }
 
   // Gets value of a key
-  async get(key) {
+  async get (key) {
     const getKeyValue = promisify(this.client.get).bind(this.client);
     const val = await getKeyValue(key);
     return val;
   }
 
   // Stores a key value in redis and expires it after given duration(seconds)
-  async set(key, value, duration) {
+  async set (key, value, duration) {
     const setKeyValue = promisify(this.client.set).bind(this.client);
     await setKeyValue(key, value);
     this.client.expire(key, duration);
   }
 
   // Deletes a key's value
-  async del(key) {
+  async del (key) {
     const delKey = promisify(this.client.del).bind(this.client);
     await delKey(key);
   }
