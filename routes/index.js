cc1c6ac285e5b48b85b8ba316b16c33a189c2d2a
Sodiq179 <babawalesodiq996@gmail.com>
2023-03-07 14:50:43 +0100

    [update]: Added new end points

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,19 @@
 import { Router } from "express";
 import AppController from "../controllers/AppController";
 import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
+import { basicAuthenticate, xTokenAuthenticate } from '../additionalUtils/auth';
+import { APIError, errorResponse } from '../additionalUtils/error';
 
 const router = Router();
 
 router.get("/status", AppController.getStatus);
 router.get("/stats", AppController.getStats);
 
+router.get('/connect', basicAuthenticate, AuthController.getConnect);
+router.get('/disconnect', xTokenAuthenticate, AuthController.getDisconnect);
+
 router.post('/users', UsersController.postNew);
+router.get('/users/me', xTokenAuthenticate, UsersController.getMe);
 
 module.exports = router;
