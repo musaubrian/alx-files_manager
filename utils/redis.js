991df531c10b9f3104b69e2a68333096b04c5d86
Sodiq179 <babawalesodiq996@gmail.com>
2023-03-09 11:54:35 +0100

    fix: isAlive() function

--- utils/redis.js
+++ utils/redis.js
@@ -2,36 +2,39 @@ import { createClient } from 'redis';
 import { promisify } from 'util';
 
 class RedisClient {
-  constructor () {
+  constructor() {
     this.client = createClient();
+    this.isClientConnected = true;
     this.client.on('error', (error) => {
       console.log(`Connection error: ${error}`);
+      this.isClientConnected = false;
+    });
+    this.client.on('connect', () => {
+      this.isClientConnected = true;
     });
   }
 
   // Checks if the connection is successfull
-  isAlive () {
-    const success = this.client.on('connect', () => true);
-    if (success) return true;
-    return false;
+  isAlive() {
+    return this.isClientConnected;
   }
 
   // Gets value of a key
-  async get (key) {
+  async get(key) {
     const getKeyValue = promisify(this.client.get).bind(this.client);
     const val = await getKeyValue(key);
     return val;
   }
 
   // Stores a key value in redis and expires it after given duration(seconds)
-  async set (key, value, duration) {
+  async set(key, value, duration) {
     const setKeyValue = promisify(this.client.set).bind(this.client);
     await setKeyValue(key, value);
     this.client.expire(key, duration);
   }
 
   // Deletes a key's value
-  async del (key) {
+  async del(key) {
     const delKey = promisify(this.client.del).bind(this.client);
     await delKey(key);
   }
